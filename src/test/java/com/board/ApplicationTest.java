1039f0440983dcf803cf1dfe863ff60abf374b5d
DaeunKang <jeangrey@xhiftcorp.com>
2019-12-05 10:00:06 +0900

    properties 파일 제외, mock+junit 추가

--- src/test/java/com/board/ApplicationTest.java
+++ src/test/java/com/board/ApplicationTest.java
@@ -2,6 +2,8 @@ package com.board;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,12 +15,51 @@ public class ApplicationTest {
     @Autowired
     private SqlSessionTemplate sqlSession;
 
-    @Test
+    @Mock
+    BillingResponse response;
+
+    //@Test
     public void contextLoads() {
     }
 
-    @Test
+    //@Test
     public void testSqlSession() throws Exception{
         System.out.println(sqlSession.toString());
+    }
+
+    @Test
+    public void example(){
+        MockitoAnnotations.initMocks(this);
+        System.out.println(((BillingResponse) response).getNullCount());
+        System.out.println(((BillingResponse) response).getClass());
+        System.out.println(((BillingResponse) response).getClass().getDeclaredFields());
+        System.out.println(((BillingResponse) response).getClass().getDeclaredFields().length);
+//        if (((BillingResponse) response).getNullCount() == ((BillingResponse) response).getClass().getDeclaredFields().length) {
+//            //throw new BillingDataNotFoundException("NO CONTENT ERROR, CHECK THE REQUEST BODY");
+//        } else if (((BillingResponse) response).getNullCount() >= 1) {
+//            //throw new PartialContentException("CONTENT IS PARTIAL, RETRY AGAIN");
+//        }
+    }
+
+//    public static <T> void validateResponseDataIsPartial(T response) throws  Exception {
+//        if (((BillingResponse) response).getNullCount() == ((BillingResponse) response).getClass().getDeclaredFields().length) {
+//            //throw new BillingDataNotFoundException("NO CONTENT ERROR, CHECK THE REQUEST BODY");
+//        } else if (((BillingResponse) response).getNullCount() >= 1) {
+//            ((BillingResponse) response).setT(response);
+//            //throw new PartialContentException("CONTENT IS PARTIAL, RETRY AGAIN");
+//        }
+//    }
+}
+
+class BillingResponse {
+    //@JsonIgnore
+    private int nullCount = 0;
+
+    public void setNullCount(int nullCount) {
+        this.nullCount += nullCount;
+    }
+
+    public int getNullCount() {
+        return nullCount;
     }
 }
